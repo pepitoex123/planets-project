6d6a0414e5cf6538f304122fb87e86d8c5d1182b
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-03 10:47:39 -0300

    Filter to find habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const parse = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return (planet["koi_disposition"] === "CONFIRMED" && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11 && planet["koi_prad"] < 1.6);
+}
 
 fs.createReadStream("kepler_data.csv")
     .pipe(parse({
@@ -9,14 +13,16 @@ fs.createReadStream("kepler_data.csv")
         columns: true
     }))
     .on("data", (data) => {
-        results.push(data);
+        if(isHabitablePlanet(data)){
+            habitablePlanets.push(data);
+        }
+
     })
     .on("error",(error) => {
         console.log(error.message);
     })
     .on("end",() => {
-        console.log(results);
-        console.log("Done!")
+        console.log(`${habitablePlanets.length} is the amount of planets found that might be habitable`);
     })
 
 
